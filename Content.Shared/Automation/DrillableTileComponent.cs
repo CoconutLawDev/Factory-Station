247d11fd8d5e3b1c4beaaabe576d4aed5e5dd1bf
CoconutLawDev <pcr9chqmft@privaterelay.appleid.com>
2026-06-17 13:41:18 +0300

    Оптимизация прототипов

--- Content.Shared/Automation/DrillableTileComponent.cs
+++ Content.Shared/Automation/DrillableTileComponent.cs
@@ -1,14 +1,15 @@
 using Robust.Shared.GameObjects;
+using Robust.Shared.GameStates;
 using Robust.Shared.Prototypes;
 using Robust.Shared.Serialization.Manager.Attributes;
 
 namespace Content.Shared.Automation;
 
-[RegisterComponent]
+[RegisterComponent, NetworkedComponent]
 public sealed partial class DrillableTileComponent : Component
 {
     [DataField]
-    public EntProtoId SpawnPrototype; // Убрано значение по умолчанию
+    public EntProtoId SpawnPrototype;
 
     [DataField]
     public int AmountPerDrill = 1;
